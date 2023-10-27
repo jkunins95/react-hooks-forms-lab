7634aa043aa9f43888ee08f2417c75dd6b334b44
+ <jkunins95@gmail.com>
2023-10-27 15:22:21 -0400

    completed deliverable 1, added search filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, selectedCategory, search, onSearchChange }) {
+
+  function handleSearch(e) {
+    onSearchChange(e.target.value)
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input 
+        type="text" 
+        name="search" 
+        placeholder="Search..."
+        value={search}
+        onChange={handleSearch}
+      />
+      <select 
+        name="filter" 
+        onChange={onCategoryChange}
+        value={selectedCategory}
+      >
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
